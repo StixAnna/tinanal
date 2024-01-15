38b0369adc6873d6c2e19605c39d5ee3326536fc
annnnna <sobnaka@mail.ru>
2024-01-16 01:48:36 +0300

    corr_of_new_activity

--- app/src/main/java/com/example/a123/SettingsActivity.kt
+++ app/src/main/java/com/example/a123/SettingsActivity.kt
@@ -23,7 +23,9 @@ class SettingsActivity : AppCompatActivity() {
         val regname : EditText = findViewById(R.id.user_name_register)
         val regtg : EditText = findViewById(R.id.user_tg_register)
         val regcity : EditText = findViewById(R.id.user_city_register)
+        val regimg : EditText = findViewById(R.id.user_img_add)
         val reggender : Spinner = findViewById(R.id.user_gender_register)
+        val regdesc : EditText = findViewById(R.id.user_descr)
         val button_reg : Button = findViewById(R.id.btn_cnfrm_setngs)
 
         button_reg.setOnClickListener{
@@ -31,8 +33,10 @@ class SettingsActivity : AppCompatActivity() {
             val telegram = regtg.text.toString().trim();
             val city = regcity.text.toString().trim();
             val gender = reggender.textAlignment.toString().trim();
+            val imgpath = regimg.text.toString().trim();
+            val desc = regdesc.text.toString().trim();
 
-            if(name == "" || telegram == "" || city == "" || gender == "")
+            if(name == "" || telegram == "" || city == "" || gender == ""|| imgpath == "" || desc == "")
                 Toast.makeText(this, "Заполните все поля!", Toast.LENGTH_LONG).show()
             else{//ELSE ADD TO DB
                 Toast.makeText(this, "**dobavlyaetsya v BD**", Toast.LENGTH_LONG).show()
