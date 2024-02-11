9c396f9ac58d721231272fd6ace85c93056f1f82
annnnna <sobnaka@mail.ru>
2024-02-11 21:27:17 +0300

    error_corr

--- app/src/main/java/com/example/a123/SettingsActivity.kt
+++ app/src/main/java/com/example/a123/SettingsActivity.kt
@@ -22,27 +22,48 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val regname : EditText = findViewById(R.id.user_name_register)
-        val regtg : EditText = findViewById(R.id.user_tg_register)
-        val regcity : EditText = findViewById(R.id.user_city_register)
-        val regimg : EditText = findViewById(R.id.user_img_add)
-        val reggender : Spinner = findViewById(R.id.user_gender_register)
-        val regdesc : EditText = findViewById(R.id.user_descr)
+        val regname : EditText = findViewById(R.id.user_name_update)
+        val regtg : EditText = findViewById(R.id.user_tg_update)
+        val regcity : EditText = findViewById(R.id.user_city_update)
+        val regimg : Button = findViewById(R.id.user_img_add_update)
+        val reggender : Spinner = findViewById(R.id.user_gender_update)
+        val regdesc : EditText = findViewById(R.id.user_descr_update)
         val button_reg : Button = findViewById(R.id.btn_cnfrm_setngs)
 
+        regimg.setOnClickListener { // Вызов метода для загрузки файла на сервер
+//            uploadFile()
+        }
         button_reg.setOnClickListener{
             val name = regname.text.toString().trim();
             val telegram = regtg.text.toString().trim();
             val city = regcity.text.toString().trim();
             val gender = reggender.textAlignment.toString().trim();
-            val imgpath = regimg.text.toString().trim();
+            // val imgpath = regimg.text.toString().trim();
             val desc = regdesc.text.toString().trim();
 
-            if(name == "" || telegram == "" || city == "" || gender == ""|| imgpath == "" || desc == "")
+            if(name == "" || telegram == "" || city == "" || gender == ""|| desc == "")
                 Toast.makeText(this, "Заполните все поля!", Toast.LENGTH_LONG).show()
             else{//ELSE ADD TO DB
                 Toast.makeText(this, "**chaning info v BD**", Toast.LENGTH_LONG).show()
             }
         }
+
     }
+
+//    private fun uploadFile() {
+//        val intent = Intent(Intent.ACTION_GET_CONTENT)
+//        intent.type = "*/*"
+//        startActivityForResult(intent, FILE_PICKER_REQUEST_CODE)
+//    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+//            val fileUri: Uri? = data?.data
+//            val file = File(fileUri?.path)
+//
+//            // Здесь вы можете выполнить загрузку файла на сервер, используя выбранный файл
+//            // Например, с помощью сторонней библиотеки, такой как OkHttp
+//        }
+//    }
 }
